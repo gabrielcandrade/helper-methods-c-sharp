e196062117f3711ab1bd44e5a6ff0c65af2d5f7d
Gabriel Andrade <gabrielcandrade01@gmail.com>
2018-03-05 14:19:44 -0300

    Subindo projeto

---
+++ HelperMethods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In what city were you born?");
+            string city = Console.ReadLine();
+
+            string reversedFirstName = ReverseString(firstName);
+            string reversedLastName = ReverseString(lastName);
+            string reversedCity = ReverseString(city);
+
+            DisplayResult(reversedFirstName, reversedLastName, reversedCity);
+
+            Console.ReadLine();
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            return String.Concat(messageArray);
+
+            /*
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);                    // This is the test, It's Working!
+            }
+            Console.Write(" ");
+            */
+        }
+
+        private static void DisplayResult(
+            string reversedFirstName, 
+            string reversedLastName, 
+            string reversedCity)
+        {
+            Console.Write(String.Format("{0} {1} {2}",
+                reversedFirstName,
+                reversedLastName,
+                reversedCity));
+        }
+    }
+}
